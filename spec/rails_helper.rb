2129bfc96f0d0b111c1ee3dc407f0b71851f254c
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2025-07-24 10:03:15 -0400

    tests fixed

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,27 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/spec/'
+  add_filter '/config/'
+  add_filter '/vendor/'
+  add_filter '/db/'
+  
+  add_group 'Models', 'app/models'
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Services', 'app/services'
+  add_group 'Views', 'app/views'
+  
+  # Engine-specific configuration
+  coverage_dir 'coverage'
+  # TODO: Increase minimum coverage as we add more tests
+  minimum_coverage 45
+  # Enable branch coverage before using refuse_coverage_drop
+  enable_coverage :branch
+  # TODO: Enable when coverage improves
+  # refuse_coverage_drop :line, :branch
+end
+
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 
@@ -13,8 +36,6 @@ require "factory_bot_rails"
 require "capybara/rails"
 require "capybara/rspec"
 require "selenium-webdriver"
-require "rails-controller-testing"
-Rails::Controller::Testing.install
 
 # Load support files
 ENGINE_ROOT = File.join(File.dirname(__FILE__), "../")
